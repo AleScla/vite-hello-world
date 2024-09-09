bcf563ee315e87ea681b2c14168a5b9fc6e67fca
AleScla <scla.alessandro@gmail.com>
2024-09-09 18:43:38 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  components:{
+    AppHeader
+  }
+  
+}
+</script>
+
+<template>
+  <AppHeader/>
+  <div>
+    <button @click="count++">Click me {{ count }}</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+      message: 'Template vite vue'
+    }
+  },
+}
+</script>
+
+<template>
+  <header>
+    <h1>
+      {{ message }}
+    </h1>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
